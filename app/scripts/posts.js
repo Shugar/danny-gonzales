a26dfa0887b7476d6bcdf5228a84948e81f065a3
Никита Головин <golovin.nik@gmail.com>
2016-06-28 18:15:42 +0300

    bubble generator - improve gen

--- app/scripts/posts.js
+++ app/scripts/posts.js
@@ -26,7 +26,10 @@ export function setPosts() {
   post.text = 'Junk Food Clothing';
   post.subtitle = 'Venice Beach, CA';
   post.background = 'url("../images/photo-2.png")';
+  posts[0] = post;
   posts[2] = post;
+  posts[13] = post;
+  posts[19] = post;
 
   post = new Post();
   post.type = POST_TYPE_PRESS;
@@ -34,22 +37,34 @@ export function setPosts() {
   post.subtitle = '- Refinery 29';
   post.background = '#0E172F';
   posts[4] = post;
+  posts[8] = post;
+  posts[12] = post;
+  posts[15] = post;
 
   post = new Post();
   post.background = 'url("../images/photo-4.png")';
+  posts[1] = post;
   posts[5] = post;
+  posts[10] = post;
+  posts[12] = post;
 
   post = new Post();
   post.type = POST_TYPE_PRESS;
   post.text = 'Wise Sons Jewish Deli Outpost at the CJM';
   post.subtitle = 'San Francisco';
   post.background = '#26386F';
+  posts[3] = post;
   posts[7] = post;
+  posts[14] = post;
+  posts[18] = post;
 
   post = new Post();
   post.type = POST_TYPE_INSTAGRAM;
   post.background = 'url("../images/photo-1.png")';
+  posts[6] = post;
   posts[9] = post;
+  posts[11] = post;
+  posts[12] = post;
 }
 
 //====================Bubbles===========
@@ -83,13 +98,13 @@ export function checkIntersection(bubble1, bubble2) {
 export function setBubbles(width) {
   let containerWidth = width;
   let coeff = 1000 / width;
-  let containerHeight = coeff * postNum * 70;
+  let containerHeight = coeff * postNum * 110;
 
   for (let i = 1; i <= postNum; i++) {
     let bubble = new Bubble();
     bubble.post = posts[i - 1];
     let ind = Math.floor(Math.random() * sizes.length);
-    bubble.size = Math.floor(sizes[ind] * (1 / coeff));
+    bubble.size = Math.floor(sizes[ind] * (coeff > 1 ? 1 : 1/coeff));
 
     let check = true;
     do {
@@ -118,6 +133,18 @@ export function setNodes(parent) {
     elm.style.top = bubble.y + 'px';
     elm.style.width = bubble.size + 'px';
     elm.style.height = bubble.size + 'px';
+
+    elm.style.background = bubble.post.background;
+
+    let elmText = document.createElement('div');
+    elmText.className = 'title';
+    elmText.innerHTML = bubble.post.text;
+    elm.appendChild(elmText);
+
+    let elmSubtitle = document.createElement('div');
+    elmText.className = 'subtitle';
+    elmText.innerHTML = bubble.post.subtitle;
+    elm.appendChild(elmSubtitle);
 
     parent.appendChild(elm);
     bubbleNodes.push(elm);
