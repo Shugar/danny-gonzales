3fbcda76c54678a85b7e3c0e8f574869192155f5
Никита Головин <golovin.nik@gmail.com>
2016-07-02 04:10:36 +0300

    art tween correction

--- app/scripts/initialize.js
+++ app/scripts/initialize.js
@@ -83,17 +83,17 @@ function scrollMagicInit() {
 
   let timeline = new TimelineLite();
   let tweens = [
-    TweenLite.to('.art-1',  1, {xPercent: -100, yPercent: 200,  ease: Power1.easeIn }),
+    TweenLite.to('.art-1',  1, {xPercent: -220, yPercent: 100,  ease: Power1.easeIn }),
     TweenLite.to('.art-2',  1, {xPercent: -150, yPercent: 52,   ease: Power1.easeIn }),
-    TweenLite.to('.art-3',  1, {xPercent: -208, yPercent: -44,  ease: Power1.easeIn }),
+    TweenLite.to('.art-3',  1, {xPercent: -208, yPercent: -56,  ease: Power1.easeIn }),
     TweenLite.to('.art-4',  1, {xPercent: -218, yPercent: -80,  ease: Power1.easeIn }),
     TweenLite.to('.art-5a', 1, {xPercent: -100, yPercent: -250, ease: Power1.easeIn }),
-    TweenLite.to('.art-5b', 1, {xPercent: -100, yPercent: -250, ease: Power1.easeIn }),
-    TweenLite.to('.art-6',  1, {xPercent: -342, yPercent: -395, ease: Power1.easeIn }),
+    TweenLite.to('.art-5b', 1, {xPercent: 200,  yPercent: -250, ease: Power1.easeIn }),
+    TweenLite.to('.art-6',  1, {xPercent: -420, yPercent: -395, ease: Power1.easeIn }),
     TweenLite.to('.art-7',  1, {xPercent: 109,  yPercent: 2,    ease: Power1.easeIn }),
-    TweenLite.to('.art-8',  1, {xPercent: 50,   yPercent: -310, ease: Power1.easeIn }),
+    TweenLite.to('.art-8',  1, {xPercent: 350,  yPercent: 150,  ease: Power1.easeIn }),
     TweenLite.to('.art-9',  1, {xPercent: 146,  yPercent: -84,  ease: Power1.easeIn }),
-    TweenLite.to('.art-10', 1, {xPercent: 235,  yPercent: 210,  ease: Power1.easeIn }),
+    TweenLite.to('.art-10', 1, {xPercent: 245,  yPercent: 210,  ease: Power1.easeIn }),
     TweenLite.to('.art-11', 1, {xPercent: 250,  yPercent: -250, ease: Power1.easeIn }),
     TweenLite.to('.art-12', 1, {xPercent: 150,  yPercent: 129,  ease: Power1.easeIn }),
     TweenLite.to('.art-13', 1, {xPercent: 336,  yPercent: 160,  ease: Power1.easeIn })
